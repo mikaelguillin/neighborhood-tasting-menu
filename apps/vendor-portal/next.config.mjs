260f13000c42eff635d7f2fcce396055782d90f4
Mikael Guillin <mikael.guillin@protonmail.com>
2026-05-07 14:14:46 -0400

    Adjust next config file for both apps

--- apps/vendor-portal/next.config.mjs
+++ apps/vendor-portal/next.config.mjs
@@ -1,8 +1,13 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
+  },
+  turbopack: {
+    root: path.resolve(process.cwd(), "../.."),
   },
   async redirects() {
     return [
